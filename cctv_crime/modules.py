191b7efedd07768b1697f7292939e18c271fc02b
jeehyun-pyun <pyun0805@naver.com>
2021-06-19 17:02:55 +0900

    code update

--- cctv_crime/modules.py
+++ cctv_crime/modules.py
@@ -5,6 +5,8 @@ import numpy as np
 import json
 import googlemaps
 from sklearn import preprocessing
+import folium
+
 
 @dataclass
 class File(object):
@@ -63,7 +65,7 @@ class Reader(ReaderBase):
         return pd.read_excel(f'{self.new_file(file)}.xls', header=header, usecols=usecols)
 
     def json(self, file) -> object:
-        return json.load(open(f'{self.new_file(file)}.json'), encoding='UTF-8')
+        return json.load(open(f'{self.new_file(file)}.json', encoding='UTF-8'))
 
     def gmaps(self) -> object:
         return googlemaps.Client(key='')
@@ -199,9 +201,9 @@ class Service(Reader):
         print(f'고령자비율과 CCTV의 상관계수 {str(cor1)} \n'
               f'외국인비율과 CCTV의 상관계수 {str(cor2)} ')
         """
-         고령자비율과 CCTV 의 상관계수 [[ 1.         -0.28078554]
+        고령자비율과 CCTV 의 상관계수 [[ 1.         -0.28078554]
                                      [-0.28078554  1.        ]] 
-         외국인비율과 CCTV 의 상관계수 [[ 1.         -0.13607433]
+        외국인비율과 CCTV 의 상관계수 [[ 1.         -0.13607433]
                                      [-0.13607433  1.        ]]
         r이 -1.0과 -0.7 사이이면, 강한 음적 선형관계,
         r이 -0.7과 -0.3 사이이면, 뚜렷한 음적 선형관계,
@@ -214,7 +216,7 @@ class Service(Reader):
                                     [-0.28078554  1.        ]]
         외국인비율 과 CCTV 상관계수 [[ 1.         -0.13607433] 거의 무시될 수 있는
                                     [-0.13607433  1.        ]]                        
-         """
+        """
         cctv_pop.to_csv('./saved_data/cctv_pop.csv')
 
     def save_police_norm(self):
@@ -242,12 +244,114 @@ class Service(Reader):
         }, inplace=True)
         x = police[self.crime_rate_columns].values
         min_max_scalar = preprocessing.MinMaxScaler()
+        """
+        스케일링은 선형변환을 적용하여
+        전체 자료의 분포를 평균 0, 분산 1이 되도록 만드는 과정
+        정규화 normalization
+        많은 양의 데이터를 처리함에 있어 데이터의 범위(도메인)를 일치시키거나
+        분포(스케일)를 유사하게 만드는 작업
+        """
+        x_scaled = min_max_scalar.fit_transform(x.astype(float))
+        police_norm = pd.DataFrame(x_scaled, columns=self.crime_columns, index=police.index)
+        police_norm[self.crime_rate_columns] = police[self.crime_rate_columns]
+        police_norm['범죄'] = np.sum(police_norm[self.crime_rate_columns], axis=1)
+        police_norm['검거'] = np.sum(police_norm[self.crime_columns], axis=1)
+        police_norm.to_csv('./saved_data/police_norm.csv', sep=',', encoding='UTF-8')
 
+    def folium_test(self):
+        file = self.file
+        reader = self.reader
+        printer = self.printer
+        file.context = './data/'
+        file.fname = 'us_unemployment'
+        state_data = reader.csv(file)
+        file.fname = 'us-states'
+        state_geo = reader.json(file)
+
+        bins = list(state_data["Unemployment"].quantile([0, 0.25, 0.5, 0.75, 1]))
+
+        m = folium.Map(location=[48, -102], zoom_start=3)
+
+        folium.Choropleth(
+            geo_data=state_geo,
+            data=state_data,
+            columns=["State", "Unemployment"],
+            key_on="feature.id",
+            fill_color="BuPu",
+            fill_opacity=0.7,
+            line_opacity=0.5,
+            legend_name="Unemployment Rate (%)",
+            bins=bins,
+            reset=True,
+        ).add_to(m)
+
+        m.save('./saved_data/Unemployment.html')
+
+    def draw_crime_map(self):
+        file = self.file
+        reader = self.reader
+        file.context = './saved_data/'
+        file.fname = 'police_norm'
+        police_norm = reader.csv(file)
+
+        file.context = './data/'
+        file.fname = 'geo_simple'
+        state_geo = reader.json(file)
+        file.fname = 'crime_in_seoul'
+        crime = reader.csv(file)
+
+        file.context = './saved_data/'
+        file.fname = 'police_pos'
+        police_pos = reader.csv(file)
+        station_names = []
+        for name in crime['관서명']:
+            station_names.append('서울' + str(name[:-1] + '경찰서'))
+        station_addrs = []
+        station_lats = []
+        station_lngs = []
+        gmaps = reader.gmaps()
+        for name in station_names:
+            temp = gmaps.geocode(name, language='ko')
+            station_addrs.append(temp[0].get('formatted_address'))
+            t_loc = temp[0].get('geometry')
+            station_lats.append(t_loc['location']['lat'])
+            station_lngs.append(t_loc['location']['lng'])
+
+        police_pos['lat'] = station_lats
+        police_pos['lng'] = station_lngs
+        col = ['살인 검거', '강도 검거', '강간 검거', '절도 검거', '폭력 검거']
+        tmp = police_pos[col] / police_pos[col].max()
+        police_pos['검거'] = np.sum(tmp, axis=1)
+
+        folium_map = folium.Map(location=[37.5502, 126.982], zoom_start=12, title='Stamen Toner')
+
+        folium.Choropleth(
+            geo_data=state_geo,
+            data=tuple(zip(police_norm['구별'],police_norm['범죄'])),
+            columns=["State", "Crime Rate"],
+            key_on="feature.id",
+            fill_color="PuRd",
+            fill_opacity=0.7,
+            line_opacity=0.2,
+            legend_name="Crime Rate (%)",
+            reset=True,
+        ).add_to(folium_map)
+
+        for i in police_pos.index:
+            folium.CircleMarker([police_pos['lat'][i], police_pos['lng'][i]],
+                                radius=police_pos['검거'][i] * 10,
+                                fill_color='#0a0a32').add_to(folium_map)
+
+        folium_map.save('./saved_data/crime_map.html')
 
 if __name__ == '__main__':
     s = Service()
     # s.save_police_pos()
     # s.save_cctv_pop()
-    s.save_police_norm()
+    # s.save_police_norm()
+    # s.folium_test()
+    s.draw_crime_map()
+
+
 
 
